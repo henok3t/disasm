83d01af2be1352829a2f609876cb97fcad4cab63
joseph mouawad <mouawad_joseph@hotmail.com>
2013-11-10 22:48:50 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,29 @@
 
 int main(int argc, char** argv)
 {
-	SymTable table;
-	vector<string> vec = table.getStringVec("test.sym");	
-	table.createSymTab(vec);	
-	table.printSymTab();	
-	
-	vector<string> vec2 = table.getStringVec("opCodes");
-	
-	if(!table.createOpTable(vec2))
-	{
-		cerr<<"OPCODE ERROR"<<endl;
-	}
-	table.printOptab();
-	
-	return 0;
+	 SymTable table;
+        vector<string> vec = table.getStringVec("test.sym");        
+        table.createSymTab(vec);        
+        table.printSymTab();        
+        
+        vector<string> vec2 = table.getStringVec("opCodes");
+        
+        if(!table.createOpTable(vec2))
+        {
+                cerr<<"OPCODE ERROR"<<endl;
+        }
+        table.printOptab();
+        
+  		ofstream sourceFile("SourceFile.sic");
+        
+        SourceCode code(table);
+    	vector<string> vec3 = code.getInstructionsHex("objCode.obj");
+    	cout<<vec3[0].print();
+    
+    	code.generateSourceCode(vec3);
+    	code.loadToFile(sourceFile);
+    
+    
+        
+        return 0;
 }

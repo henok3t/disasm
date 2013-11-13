02a86432ce873b53d669cf3388f016980109f2b0
joseph mouawad <mouawad_joseph@hotmail.com>
2013-11-12 17:23:53 -0800

    Update Instruction.hpp
    commented

--- Instruction.hpp
+++ Instruction.hpp
@@ -12,23 +12,25 @@ using namespace std;
 class Instruction
 {
 	public:
-	Instruction(){};
-	Instruction(string opKey, SymTable Stable);
-	~Instruction(){};
+	Instruction(){};      // default Constructor
+	Instruction(string opKey, SymTable Stable);// constructor with an opcode key and a
+						// symTable instance containig the
+						// corresponding Optable and Symtable
+	~Instruction(){};  // default destructor
 	
-	string GetName();
+	string GetName();    // returns the Mneumonic of the instruciton
 	string GetOperand();
-	int GetFormat();
-	void SetFormat(int);
-	void SetOperand(string op);
+	int GetFormat();// returns the format of the instruction (format 1,2, 3, or 4)
+	void SetFormat(int); // set the format of the instruction (format 1,2, 3, or 4)
+	void SetOperand(string op);// set the operand, what comes after the mneumonic in the source code
 	int GetLength();
 	
 private:
-	SymTable Stable;
-	string opKey;
-	string name;
-	int format;
-	string operand;
+	SymTable Stable; 
+	string opKey; // opOCde
+	string name; // Mneunmonic
+	int format;  //  format 
+	string operand;  // operand, what comes after the mneumonic in the source code
 };
 
 #endif // INSTRUCTION_HPP
